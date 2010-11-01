feab9e09d5044a337fd95fa9d351699824b4da9e
Pouya Rezaeifakhr <pouya@lavabit.com>
2010-11-01 12:20:06 -0700

    updated test for PhotoSize object

--- lib/flickr_mocks/models/photo_size.rb
+++ lib/flickr_mocks/models/photo_size.rb
@@ -2,6 +2,7 @@ module FlickrMocks
 
   class PhotoSize
     def initialize(size)
+      raise TypeError, 'FlickRaw::Response expected' if size.is_a? FlickRaw::ResponseList
       raise TypeError, 'FlickRaw::Response expected' unless size.is_a? FlickRaw::Response
       @delegated_to_object= size
     end
@@ -22,35 +23,32 @@ module FlickrMocks
       @delegated_to_object == other.instance_eval('@delegated_to_object')
     end
 
+    # metaprogramming methods
+    def delegated_instance_methods
+      @delegated_to_object.methods(false).push(:flickr_type)
+    end
+
+    def method_missing(id,*args,&block)
+      return @delegated_to_object.send(id,*args,&block) if delegated_instance_methods.include?(id)
+      super
+    end
+
+    alias :old_respond_to? :respond_to?
+    def respond_to?(method,type=false)
+      return true if delegated_instance_methods.include?(method)
+      old_respond_to?(method,type)
+    end
+
+    alias :old_methods :methods
+    def methods
+      delegated_instance_methods + old_methods
+    end
+
+    # cloning and copying methods
     def initialize_copy(orig)
       super
       @delegated_to_object = @delegated_to_object.clone
     end
-
-    def method_missing(id,*args,&block)
-      return @delegated_to_object.send(id,*args,&block) if delegated_methods.include?(id)
-      super
-    end
-
-    def respond_to?(method,type=false)
-      return true if delegated_methods.include?(method)
-      super
-    end
-
-    def methods
-      delegated_methods + super
-    end
-
-    def public_methods(all=true)
-      delegated_methods + super(all)
-    end
-
-    def delegated_methods
-     @delegated_to_object.methods(false).push(:flickr_type)
-    end
-    
   end
-
-
 
 end
--- spec/models/photo_size_spec.rb
+++ spec/models/photo_size_spec.rb
@@ -1,82 +1,122 @@
 require 'spec_helper'
 
 describe APP::PhotoSize do
-    let(:klass){APP::PhotoSize}
-    let(:fixtures){ APP::Fixtures.new}
-    let(:size_fixture){fixtures.photo_size}
-    let(:expected_methods){fixtures.expected_methods.photo_size}
-    let(:size){klass.new size_fixture}
+  let(:klass){APP::PhotoSize}
+  let(:fixtures){ APP::Fixtures.new}
+  let(:size_fixture){fixtures.photo_size}
 
+  subject {klass.new size_fixture}
 
-  describe "class" do
-    it "should return a proper class" do
-      size.class.should == klass
+  context "#initialize" do
+    context "size fixture" do
+      it "returns object of proper class" do
+        subject.should be_a(APP::PhotoSize)
+      end
     end
-  end
-
-  describe "size" do
-    it "Should respond to :size" do
-      size.should respond_to :size
+    context "FlickRaw::ResponseList object" do
+      it "raises error" do
+        expect {
+          klass.new(fixtures.photos)
+        }.to raise_error(TypeError)
+      end
     end
-    it "should return proper size" do
-      size.size.should == size_fixture.label.downcase.sub('_',' ').to_s
-    end
-  end
-
-  describe "id" do
-    it "should respond to :id" do
-      size.should respond_to :id
-    end
-    it "should return proper :id" do
-      size.id.should == size_fixture.source.split('/')[-1].split('_')[0]
-    end
-  end
-
-  describe "secret" do
-    it "should respond :secret" do
-      size.should respond_to :secret
-    end
-    it "should return :secret" do
-      size.secret.should == size_fixture.source.split('/')[-1].split('_')[1]
-    end
-  end
-
-  describe "delegation methods" do
-    it "should delegate to FlickRaw::Response" do
-      expected_methods.each do |method|
-        size.send(method).should == size_fixture.send(method)
+    context "unexpected object" do
+      it "raises error when array object provided" do
+        expect{
+          klass.new([])
+        }.to raise_error(TypeError)
+      end
+      it "raises error when Hash object provided" do
+        expect{
+          klass.new({})
+        }.to raise_error(TypeError)
       end
     end
   end
 
-  describe "==" do
+  context "instance methods" do
+    specify {subject.should respond_to(:size)}
+    describe "#size" do
+      it "should return proper size" do
+        subject.size.should == size_fixture.label.downcase.sub('_',' ').to_s
+      end
+    end
 
-    it "should be == to clone of itself" do
-      size.should == size.clone
+    specify {subject.should respond_to(:id)}
+    describe "#id" do
+      it "should return proper :id" do
+        subject.id.should == size_fixture.source.split('/')[-1].split('_')[0]
+      end
     end
-    it "should not == to another class" do
-      size.should_not == [1,2,3,4]
+
+    specify {subject.should respond_to(:secret)}
+    describe "#secret" do
+      it "should return :secret" do
+        subject.secret.should == size_fixture.source.split('/')[-1].split('_')[1]
+      end
     end
-    it "should detect a single attribute error" do
-      size.delegated_methods do |method|
-        value = case size.send(method)
-        when String then Faker::Lorem.sentence(3)
-        when FixNum then Random.rand
-        else size.send(method)
+
+
+    specify{subject.should respond_to(:==)}
+    context "#==" do
+      it "returns true when object is compared to clone of itself" do
+        subject.should == subject.clone
+      end
+      it "returns true when object is compared to an array" do
+        subject.should_not == [1,2,3,4]
+      end
+      it "returns false when compared to object that is different in one attribute" do
+        subject.delegated_instance_methods do |method|
+          value = case subject.send(method)
+          when String then Faker::Lorem.sentence(3)
+          when FixNum then Random.rand
+          else Random.rand
+          end
+          other = subject.clone
+          other.stub(method).returns(value)
+          subject.should_not == other
         end
-        other = size.clone
-        other.stub(method).returns(value)
-        size.should_not == other
       end
     end
   end
 
-  describe "clone" do
-    it "should be have unique ids compared to clone" do
-      subject = size
-      other = subject.clone
-      subject.instance_eval('@delegated_to_object.__id__').should_not == other.instance_eval('@delegated_to_object.__id__')
+  context "metaprogramming methods" do
+
+    context "#delegated_instance_methods" do
+      it "should delegate to FlickRaw::Response" do  
+        subject.delegated_instance_methods.sort.should == fixtures.expected_methods.photo_size.sort
+      end
+    end
+
+    context "#respond_to?" do
+      it "returns true when given on of delegated_instance methods" do
+        subject.delegated_instance_methods.each do |method|
+          subject.should respond_to(method)
+        end
+      end
+      it "returns true when given expected methods" do
+        subject.methods do |method|
+          subject.should respond_to(method)
+        end
+      end
+    end
+
+    context "#methods" do
+      it "returns list that includes the methods that are delegated" do
+        expected_methods = subject.old_methods + fixtures.expected_methods.photo_size
+        (subject.methods - expected_methods).should be_empty
+        subject.methods.length.should == expected_methods.length
+      end
+
     end
   end
-
+  
+  context "custom cloning methods" do
+    context "#initialize_copy" do
+      it "returns photo object that have distinct __id__ as compared with original" do
+        other = subject.clone
+        subject.instance_eval('@delegated_to_object.__id__').should_not == other.instance_eval('@delegated_to_object.__id__')
+      end
+    end
+  end
 end
